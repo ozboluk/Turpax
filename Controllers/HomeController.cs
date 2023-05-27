9e5a0c5eaebd9188f9c288f9c28b7353c9a95c2b
ozboluk <ibrahimozboluk@gmail.com>
2023-05-27 15:36:12 +0300

    Turpax
    Otomasyon Sistemi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Turpax.Models;
+
+namespace Turpax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Turpax turpax)
+        {
+            using (ozbolukEntities db = new ozbolukEntities())
+            {
+                string rasgele = Guid.NewGuid().ToString().Substring(0, 6);
+                Models.Turpax turpax1 = new Models.Turpax
+                {
+                    Filo_kodu = rasgele,
+                    Istasyon_fiyati = turpax.Istasyon_fiyati,
+                    Plaka = turpax.Plaka,
+                    Satis_fiyati = turpax.Satis_fiyati,
+                   
+                    Litre = turpax.Litre,
+                    Tarih_Saat = turpax.Tarih_Saat,
+                    Tutar = turpax.Tutar,
+                    Satis_tipi ="Otomasyon",
+                    Pompa = "00"+turpax.Pompa,
+                    Pompaci = "öksüz48",
+                    Urun_adi = turpax.Urun_adi
+                };
+                db.Turpax.Add(turpax1);
+                db.SaveChanges();
+            }
+           
+           
+            return View();
+        }
+        public ActionResult Index(string id)
+        {
+            using (ozbolukEntities db = new ozbolukEntities())
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    return View(db.Turpax.Where(x=>x.Plaka.Contains(id)).ToList());
+                }
+                return View(db.Turpax.ToList());
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (ozbolukEntities db = new ozbolukEntities())
+            {
+               var sa= db.Turpax.FirstOrDefault(x => x.Id==id);
+                db.Turpax.Remove(sa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+          
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Turpax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Turpax")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c67a0656-f486-43e6-8b40-6b4ece2dd3df")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
